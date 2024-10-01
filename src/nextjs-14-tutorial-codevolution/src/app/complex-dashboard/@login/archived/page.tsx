e9a7a98b869abd40db1c08c8b0fdd05e50f2e49b
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-10-01 22:32:13 +0900

    Finished Chapter #30.

---
+++ src/nextjs-14-tutorial-codevolution/src/app/complex-dashboard/@login/archived/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Card from '@/components/card';
+
+const ArchivedLogins = () => {
+  return <Card>Please login to continue.</Card>;
+};
+
+export default ArchivedLogins;
