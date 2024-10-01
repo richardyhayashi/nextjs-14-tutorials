37ff74d7dc5c226e84b445a44327d1a40e396140
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-10-01 21:28:10 +0900

    Finished Chapter #28.

---
+++ src/nextjs-14-tutorial-codevolution/src/app/complex-dashboard/@revenue/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Card from '@/components/card';
+
+const RevenueMetricsMetrics = () => {
+  return <Card>Revenue MetricsMetrics</Card>;
+};
+
+export default RevenueMetricsMetrics;
