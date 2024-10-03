d2cb5feffcb9a26cf0e78c7817f5f807aa75bb1d
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-10-03 13:24:22 +0900

    Finished Chapter #36.

--- src/nextjs-14-tutorial-codevolution/src/app/comments/route.ts
+++ src/nextjs-14-tutorial-codevolution/src/app/comments/route.ts
@@ -1,9 +1,11 @@
 import { comments } from "./data";
 
+// GET 
 export async function GET() {
   return Response.json(comments);
 }
 
+// POST
 export async function POST(request: Request) {
   const comment = await request.json();
 
